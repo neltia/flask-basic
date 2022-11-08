18733779d3b61fb5e425a371f9d6a001f0e2d780
neltia <dsz08082@naver.com>
2022-11-08 16:42:36 +0900

    werkzeug update & indent update

--- 01_File_Upload/app.py
+++ 01_File_Upload/app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template, request
-from werkzeug import secure_filename
+from flask import Flask
+from flask import render_template
+from flask import request
 import os
+try:
+	from werkzeug.utils import secure_filename
+except:
+	from werkzeug import secure_filename
+
+
 app = Flask(__name__)
 #app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #파일 업로드 용량 제한 단위:바이트
+
 
 #HTML 렌더링
 @app.route('/')
 def home_page():
 	return render_template('home.html')
+
 
 # 파일 리스트
 @app.route('/list')
@@ -17,10 +26,12 @@ def list_page():
 	html += "file_list: {}".format(file_list) + "</center>"
 	return html
 
+
 #업로드 HTML 렌더링
 @app.route('/upload')
 def upload_page():
 	return render_template('upload.html')
+
 
 #파일 업로드 처리
 @app.route('/fileUpload', methods = ['GET', 'POST'])
@@ -31,6 +42,7 @@ def upload_file():
 		f.save('./uploads/' + secure_filename(f.filename))
 		return render_template('check.html')
 
+	
 #서버 실행
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080, debug = True)
